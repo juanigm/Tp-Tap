7f3339804e5001f5109a205efd186ad38a049092
maaxifig <maxifigueroa2012@gmail.com>
2021-10-30 21:03:10 -0300

    Created login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
 from flask_socketio import SocketIO, send
 
@@ -9,30 +9,41 @@ socketio = SocketIO(app)
 #Conection from bd
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Ignacio321'
+app.config['MYSQL_PASSWORD'] = 'Loreto18'
 app.config['MYSQL_DB'] = 'tp-tap'
 
 mysql = MySQL(app)
 
 
 #Routes
-@app.route('/users', methods=['GET'])
-def users():
-    cursor = mysql.connection.cursor()
-    cursor.execute('SELECT * FROM usuario')
-    data = cursor.fetchall()
-    print(data)
-    return render_template('index.html', users = data)
 
-@app.route('/chat', methods=['POST'])
+@app.route('/login', methods=['POST','GET'])
+def users():
+    data = ''
+    if(request.method == 'POST'):
+        userId = request.form['userid']
+        password = request.form['password']
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT userId, password FROM usuario WHERE userid = %s AND password = %s', (userId,password))
+        data = cursor.fetchall()
+        print('DATA: '+str(data))
+    if(data):
+        return redirect(url_for('chats')) 
+    else:
+        return render_template('index.html')
+
+@app.route('/chat')
 def chats():
-    if request.method == 'POST':
-        myMessage = request.form['myMessage']
-        print(myMessage)
+   # if request.method == 'POST':
+       # myMessage = request.form['myMessage']
+     #   print(myMessage)
        # cursor = mysql.connection.cursor()
         #cursor.execute('INSERT INTO mensaje (mensaje) VALUES (%s)', (myMessage))
         #mysql.connection.commit()
     return render_template('chats.html')
+
+""" def login(): """
+
 
 @socketio.on('message')
 def handleMessage(msg):
